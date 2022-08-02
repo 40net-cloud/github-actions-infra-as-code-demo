6af5755c156f8e571c79fa7aa7651e8cc179c973
Joeri <jvanhoof@fortinet.com>
2022-08-02 09:37:29 +0200

    Update formatting

--- Azure/Day1-Network/01-network.tf
+++ Azure/Day1-Network/01-network.tf
@@ -70,7 +70,7 @@ resource "azurerm_local_network_gateway" "onpremise" {
   name                = "${var.PREFIX}-VPN-Sophia"
   location            = azurerm_resource_group.resourcegroup.location
   resource_group_name = azurerm_resource_group.resourcegroup.name
-  gateway_fqdn     = "gorgon.jvh.be"
+  gateway_fqdn        = "gorgon.jvh.be"
   address_space       = ["172.16.251.0/24"]
 }
 
