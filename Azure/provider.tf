33143359a40f9cc8e3e2eee7a95f686bc2bb08fe
rhr1987 <118000755+rhr1987@users.noreply.github.com>
2024-06-19 15:20:37 +0200

    Revert "checking infracost"

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -25,7 +25,7 @@ terraform {
   }
 
   backend "remote" {
-    organization = "rrother_fortinet"
+    organization = "40net-cloud"
 
     workspaces {
       name = "github-actions-infra-as-code-demo-azure"
